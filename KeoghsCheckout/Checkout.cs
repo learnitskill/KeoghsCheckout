921a131db01a2145011bf7c1f8e426a36540f4b8
shehz <learnitskill@gmail.com>
2020-08-07 12:44:37 +0100

    write test to calculate basket total amount

---
+++ KeoghsCheckout/Checkout.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeoghsCheckout
+{
+    public class Checkout
+    {
+        public object BasketTotalAmount(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KeoghsCheckoutTest/CheckoutTest.cs
@@ -0,0 +1,26 @@
+﻿using KeoghsCheckout;
+using NUnit.Framework;
+using System;
+
+namespace KeoghsCheckoutTest
+{
+    public class CheckoutTest
+    {
+        [Test]
+        public void BasketTotalAmount_GivenBasket_BaksetTotalAmountShouldBe10()
+        {
+            //Arrange
+            var expected = 10;
+            var checkout = new Checkout();                    
+            var basket = new Basket();
+            var itemA = new BasketItem("A");            
+            basket.Add(itemA);
+
+            //Act
+            var total = checkout.BasketTotalAmount(basket);
+
+            //Assert
+            Assert.That(total, Is.EqualTo(expected));
+        }
+    }
+}
