2ace1d572a55ed425a3e31b4294482c2d674cc14
shehz <learnitskill@gmail.com>
2020-08-07 13:25:34 +0100

    refactor

--- KeoghsCheckout/Checkout.cs
+++ KeoghsCheckout/Checkout.cs
@@ -15,14 +15,17 @@ namespace KeoghsCheckout
         public int BasketTotalAmount(IEnumerable<BasketItem> basketItems)
         {
             int total = 0;
-            foreach (var item in basketItems)
+        
+            var groupedItems = basketItems.GroupBy(x => x.itemSKU);
+            foreach (var item in groupedItems)
             {
-                if (basketItems.Count(x=>x.itemSKU=="B") == 3)
+                if(item.Count() ==3)
                     continue;
-                total += priceEngine.GetPrice(item.itemSKU);
-            }
 
-                if (basketItems.Count(x=>x.itemSKU=="B") == 3)
+                total += priceEngine.GetPrice(item.Key) * item.Count();
+            }
+             
+            if (basketItems.Count(x=>x.itemSKU=="B") == 3)
                 total += 40;
 
             return total;
